91c2828a51455745e66bae617346b636db8de142
SaxenaMudita <saxena.mudita987@gmail.com>
2025-04-10 13:54:11 +0530

    refactor code to improve readability

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -4,34 +4,49 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    delimiter, numbers = extract_custom_delimiter(numbers)
-    nums = split_numbers(numbers, delimiter)
+    delimiter, numbers = parse_delimiters(numbers)
+    nums = parse_numbers(numbers, delimiter)
     validate_no_negatives(nums)
     nums.sum
   end
 
   private
 
-  def self.extract_custom_delimiter(numbers)
-    if numbers.start_with?("//[")
-      delimiters = numbers.scan(/\[(.*?)\]/).flatten
-      delimiter_regex = delimiters.map { |d| Regexp.escape(d) }.join("|")
-      numbers = numbers.split("\n", 2).last
-      [ delimiter_regex, numbers ]
-    elsif numbers.start_with?("//")
-      delimiter, numbers = numbers[2..].split("\n", 2)
-      [ Regexp.escape(delimiter), numbers ]
+  def self.parse_delimiters(numbers)
+    if custom_delimiters?(numbers)
+      parse_multiple_delimiters(numbers)
+    elsif single_custom_delimiter?(numbers)
+      parse_single_delimiter(numbers)
     else
       [ DEFAULT_DELIMITERS, numbers ]
     end
   end
 
-  def self.split_numbers(numbers, delimiter)
+  def self.custom_delimiters?(numbers)
+    numbers.start_with?("//[")
+  end
+
+  def self.single_custom_delimiter?(numbers)
+    numbers.start_with?("//")
+  end
+
+  def self.parse_multiple_delimiters(numbers)
+    delimiters = numbers.scan(/\[(.*?)\]/).flatten
+    delimiter_regex = delimiters.map { |d| Regexp.escape(d) }.join("|")
+    [ delimiter_regex, numbers.split("\n", 2).last ]
+  end
+
+  def self.parse_single_delimiter(numbers)
+    delimiter, remaining_numbers = numbers[2..].split("\n", 2)
+    [ Regexp.escape(delimiter), remaining_numbers ]
+  end
+
+  def self.parse_numbers(numbers, delimiter)
     numbers.split(/#{delimiter}/).map(&:to_i).reject { |n| n > 1000 }
   end
 
   def self.validate_no_negatives(nums)
-    negatives = nums.select { |n| n < 0 }
+    negatives = nums.select(&:negative?)
     raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
   end
 end
